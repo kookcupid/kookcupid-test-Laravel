81ac214efaaae2c1b1ff3af704cf18a4ec054b96
kookcupid <kookcupid@gmail.com>
2021-08-15 14:10:24 +0700

    gooo

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin page</title>
+</head>
+<body>
+
+    <?php
+
+    $user ="kookcupid";
+    $arr = array("Home","Member","About","Contact");
+
+    ?>
+
+    @if($user =="Natthapon")
+        <h1>ยินดีตอนรับ Admin{{@user}}</h1>
+        <p>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
+        <h1>{{$user}}</h1>
+    @else
+        <p>ผู้ใช้ท่านนี้ไม่ได้เป็น Admin</p>
+    @endif
+
+    @foreach($arr as $menu)
+        <a href="">{{$menu}}</a>
+    @endforeach
+
+    <ul>
+    @for($i=1;$i<=10;$i++)
+        <li?>{{$i}}</li>
+    @endfor
+    </ul>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+}); 
+
+Route::get('/member', function () {
+    return view('member.index');
+}); 
+
+Route::get('/admin', function () {
+    return view('admin.index');
+}); 
